6e6576f072358ec59788910f18e7d75e5edc8cc4
fagnerviana <fagsantiago@gmail.com>
2022-09-13 11:50:59 -0300

    Conevrsão de String para Array e vice-versa

--- src/formacaoJavaWebFullStack/executavelArrayVetor/SplitArray.java
+++ src/formacaoJavaWebFullStack/executavelArrayVetor/SplitArray.java
@@ -1,5 +1,8 @@
 package formacaoJavaWebFullStack.executavelArrayVetor;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SplitArray {
 
 	public static void main(String[] args) {
@@ -8,9 +11,34 @@ public class SplitArray {
 		String[] valoresArray = texto.split(",");
 		
 		
+		
+		System.out.println(" String para Arrays ");
 		for(int i =0 ; i<valoresArray.length;i++) {
 			System.out.println("Posição: "+i+"- "+ valoresArray[i]);	
 		}
+		
+		//Convertendo um Array em uma lista
+		
+		List<String> list = Arrays.asList(valoresArray);
+		
+		System.out.println(" Array para Lista ");
+		for (String valorString : list) {
+			System.out.println(valorString);
+		}
+		
+		
+		//Converter Lista para Array
+		
+		String[]  conversaoArray = list.toArray(new String[list.size()]);
+		
+		System.out.println(" Lista para Array ");
+		System.out.println(conversaoArray);
+		
+		for(int l=0;l<conversaoArray.length;l++) {
+			System.out.println(" "+conversaoArray[l].toUpperCase());
+		
+		}
+		
 		
 	
 	}
