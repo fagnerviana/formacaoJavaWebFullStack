2b10bc36fb801e18789868f506e007e8747298a5
fagnerviana <fagsantiago@gmail.com>
2022-09-20 15:27:04 -0300

    Trabalhando com Instant em Java

--- src/formacaoJavaWebFullStack/datas/ManipulandoData.java
+++ src/formacaoJavaWebFullStack/datas/ManipulandoData.java
@@ -2,7 +2,11 @@ package formacaoJavaWebFullStack.datas;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
 import java.util.Calendar;
+import java.util.Date;
 
 public class ManipulandoData {
 
@@ -23,6 +27,30 @@ public class ManipulandoData {
 		calendar.add(calendar.YEAR, 3);
 		
 		System.out.println(new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+		
+		
+		/*Contas os dias entre datas */
+		
+		//Retorna o numero de dias entre as datas informadas
+		long dias = ChronoUnit.DAYS.between(LocalDate.parse("1985-07-05"),LocalDate.now());
+		
+		//Retorna a quantidade de decadas entre as datas infomadas
+		long decadas =  ChronoUnit.DECADES.between(LocalDate.parse("1985-07-05"), LocalDate.now());
+		
+		//Retona a quantidade de horas entre as datas passadas
+		long meses =  ChronoUnit.MONTHS.between(LocalDate.parse("1985-07-05"), LocalDate.now());
+		
+		//Retona a quantidade de anos
+		long anos =  ChronoUnit.YEARS.between(LocalDate.parse("1985-07-05"), LocalDate.now());
+		
+		
+		System.out.println("Total de dias: "+dias);
+		
+		System.out.println("Decadas: "+decadas);
+		
+		System.out.println("Meses: "+meses);
+		
+		System.out.println("Anos:"+anos );
 		
 		
 	}
