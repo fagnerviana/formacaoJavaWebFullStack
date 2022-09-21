d2da47d803146c37030871a9722699bf6d46eb67
fagnerviana <fagsantiago@gmail.com>
2022-09-20 21:22:49 -0300

    Gerando parcelas mensai e suas datas

---
+++ src/formacaoJavaWebFullStack/datas/ParcelasVencimento.java
@@ -0,0 +1,21 @@
+package formacaoJavaWebFullStack.datas;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class ParcelasVencimento {
+
+	public static void main(String[] args) {
+		
+		LocalDate dataBase = LocalDate.parse("2019-10-05");
+		
+		for(int mes=1;mes<=12;mes++) {
+			dataBase = dataBase.plusMonths(1);
+			
+			System.out.println("Data de vencimento do boleto : "+
+			dataBase.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))+ "do mês :"+mes);
+		}
+
+	}
+
+}
